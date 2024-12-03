d3fa621caad90a79a2035dd06b178ef4d7da4f86
Agon <agon-kolgeci@hotmail.com>
2024-12-03 15:58:40 +0100

    Create template.cpp

---
+++ Training/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+const int INF = 1e18;
+const int MOD = 1e9 + 7;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+}
